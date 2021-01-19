a396491097314464e8bc1ee022b784c0a116c5df
joethyr <joethayr@gmail.com>
2021-01-18 19:17:20 -0700

    added basic math functions on js.

---
+++ script.js
@@ -0,0 +1,36 @@
+// function that adds
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return subtract(a, b);
+      break;
+    case "*":
+      return multiply(a, b);
+      break;
+    case "/":
+      if (b === 0) return null;
+      return divide(a, b);
+      break;
+    default:
+      return null;
+  }
+}
