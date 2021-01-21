c296e4a571e78fbc514bb892e43cda17061a0cfc
joethyr <joethayr@gmail.com>
2021-01-20 21:34:49 -0700

    added jquery to get the value of the button and store it in a variable when clicked.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// let buttonZero = document.getElementsByClassName("button-zero");
+// let buttonOne = document.getElementsByClassName("button-one");
+// let buttonTwo = document.getElementsByClassName("button-two");
+// let buttonThree = document.getElementsByClassName("button-three");
+// let buttonFour = document.getElementsByClassName("button-four");
+// let buttonFive = document.getElementsByClassName("button-five");
+// let buttonSix = document.getElementsByClassName("button-six");
+// let buttonSeven = document.getElementsByClassName("button-seven");
+// let buttonEight = document.getElementsByClassName("button-eight");
+// let buttonNine = document.getElementsByClassName("button-nine");
+// let buttonDecimal = document.getElementsByClassName("button-decimal");
+
+let outputNumbers = document.getElementsByClassName("output-numbers");
+
 // function that adds
 function add(a, b) {
   return a + b;
@@ -34,3 +48,12 @@ function operate(operator, a, b) {
       return null;
   }
 }
+let displayValue;
+
+$(document).ready(function () {
+  $("button.button-operand").click(function () {
+    displayValue = $(this).val();
+    // will probably store the values in as an array/object
+    // how to store the values without the previous one being overwritten
+  });
+});
