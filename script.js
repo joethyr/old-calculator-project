f9bcb6e9f6695df6cc57e0143e7807289e9dd89f
joethyr <joethayr@gmail.com>
2021-01-21 19:00:56 -0700

    fixed buton function. Able to output the the value of the specific button clicked.

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// let buttonZero = document.getElementsByClassName("button-zero");
-// let buttonOne = document.getElementsByClassName("button-one");
-// let buttonTwo = document.getElementsByClassName("button-two");
-// let buttonThree = document.getElementsByClassName("button-three");
-// let buttonFour = document.getElementsByClassName("button-four");
-// let buttonFive = document.getElementsByClassName("button-five");
-// let buttonSix = document.getElementsByClassName("button-six");
-// let buttonSeven = document.getElementsByClassName("button-seven");
-// let buttonEight = document.getElementsByClassName("button-eight");
-// let buttonNine = document.getElementsByClassName("button-nine");
-// let buttonDecimal = document.getElementsByClassName("button-decimal");
-
-let outputNumbers = document.getElementsByClassName("output-numbers");
-
 // function that adds
 function add(a, b) {
   return a + b;
@@ -48,12 +34,15 @@ function operate(operator, a, b) {
       return null;
   }
 }
-let displayValue;
+let displayValue = 0;
 
 $(document).ready(function () {
   $("button.button-operand").click(function () {
     displayValue = $(this).val();
     // will probably store the values in as an array/object
     // how to store the values without the previous one being overwritten
+    document.getElementById("output-numbers").innerHTML = displayValue;
   });
 });
+
+function Testm() {}
